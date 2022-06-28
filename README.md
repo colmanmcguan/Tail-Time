7596030f7f1dd8dc51f361715239ca50930f7c64
Colman McGuan <colmanmcguan@sputnik.local>
2022-06-28 19:06:23 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tail Time
+
+Tail Time is a website fingerprinting defense built on top of Walkie-Talkie.
---
+++ browser/popup/config_popup.js
@@ -0,0 +1,31 @@
+/* load in whatever the current config is */
+var ffwindow = browser.extension.getBackgroundPage();
+
+var config = JSON.parse(ffwindow.getConfig());
+
+document.getElementById("enabled").checked = config.enabled;
+document.getElementById("timeout").value = config.timeout;
+
+/* listen for changes */
+document.getElementById("upload").addEventListener("click", (e) => {
+    var ffwindow = browser.extension.getBackgroundPage();
+
+    newTimeout = parseInt(document.getElementById("timeout").value);
+
+    /* catch invalid timeout values */
+    if (newTimeout < 0 || isNaN(newTimeout)) 
+        newTimeout = 0;
+
+    /* somebody disabled tt but provided a timeout value anyways.. */
+    if (!document.getElementById("enabled").checked)
+        newTimeout = 0;
+
+    config = {
+        enabled: document.getElementById("enabled").checked,
+        timeout: newTimeout
+    };
+
+    /* set the new config */
+    ffwindow.setConfig(JSON.stringify(config));
+});
+
